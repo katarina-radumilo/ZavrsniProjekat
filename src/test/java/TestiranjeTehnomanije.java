871b860ae0878c0240ce95c19d2d62fb6afa12cf
Katarina Radumilo <katarina.radumilo@gmail.com>
2021-07-11 22:07:49 +0200

    Dodati test scenariji

--- src/test/java/TestiranjeTehnomanije.java
+++ src/test/java/TestiranjeTehnomanije.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
+import java.util.List;
+import java.util.Locale;
 
 public class TestiranjeTehnomanije {
     private WebDriver driver;
@@ -26,15 +28,95 @@ public class TestiranjeTehnomanije {
 
     @Test
     public void kupiNajjeftinijiFen() {
+        postaviPocetnoStanje();
+
+        odaberiKategorijuProizvoda("lepota-i-zdravlje", "fenovi-za-kosu");
+
+        sortirajPoCeniRastuce();
+
+        dodajPrviProizvodUKorpu();
+
+        pregledajKorpu();
+    }
+
+    @Test
+    public void dodajNajjeftinijuPeglu() {
+        postaviPocetnoStanje();
+
+        odaberiKategorijuProizvoda("mali-kucni-aparati", "standardne-pegle");
+
+        sortirajPoCeniRastuce();
+
+        dodajPrviProizvodUKorpu();
+
+        pregledajKorpu();
+    }
+
+    private void postaviPocetnoStanje() {
+        vratiNaPocetnuStranu();
+
         ukloniPopupove();
 
         ulogujKorisnika();
+    }
 
+    private void vratiNaPocetnuStranu() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//[@href='https://www.tehnomanija.rs/']"))));
+            WebElement continueShopping = driver.findElement(By.xpath("//[@href='https://www.tehnomanija.rs/']"));
+            actions.moveToElement(continueShopping).perform();
+            continueShopping.click();
+        }
+        catch (Exception e) {
 
+        }
+    }
+
+    private void odaberiKategorijuProizvoda(String kategorija, String podKategorija) {
+        WebElement categoryMenu = driver.findElement(By.className("category-menu"));
+        actions.moveToElement(categoryMenu).perform();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[@href='https://www.tehnomanija.rs/" + kategorija + "']"))));
+        WebElement productCategory = driver.findElement(By.xpath("//a[@href='https://www.tehnomanija.rs/" + kategorija + "']"));
+        actions.moveToElement(productCategory).perform();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[@href='https://www.tehnomanija.rs/" + kategorija + "/" + podKategorija + "']"))));
+        WebElement productSubCategory = driver.findElement(By.xpath("//a[@href='https://www.tehnomanija.rs/" + kategorija + "/" + podKategorija + "']"));
+        productSubCategory.click();
+    }
+
+    private void sortirajPoCeniRastuce() {
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("sort-products"))));
+        WebElement sortProducts = driver.findElement(By.className("sort-products"));
+        actions.moveToElement(sortProducts).perform();
+        sortProducts.click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@data-value='price_asc']"))));
+        WebElement sortCategory = driver.findElement(By.xpath("//div[@data-value='price_asc']"));
+        sortCategory.click();
+    }
+
+    private void dodajPrviProizvodUKorpu() {
+        List<WebElement> productList = (List<WebElement>) driver.findElements(By.xpath("//div[@class='product-wrap-grid js-product-ga-wrap']//div[@class='no-display js-ga-product-data']"));
+        WebElement firstProductItem = productList.stream().findFirst().get();
+        String firstProductID = firstProductItem.getAttribute("data-product-id");
+
+        WebElement addToBasket = driver.findElement(By.xpath("//a[@data-blue-product-product-id='" + firstProductID + "']"));
+        actions.moveToElement(addToBasket).perform();
+        addToBasket.click();
+    }
+
+    private void pregledajKorpu() {
+        WebElement goToBasket = driver.findElement(By.className("basket-header"));
+        goToBasket.click();
     }
 
     private void ulogujKorisnika() {
         WebElement loginDropdown = driver.findElement(By.className("user-name"));
+        String loginDropdownCaption = loginDropdown.getText().toUpperCase().trim();
+        if (!loginDropdownCaption.equals("PRIJAVA")) {
+            return;
+        }
 
         actions.moveToElement(loginDropdown).perform();
 
